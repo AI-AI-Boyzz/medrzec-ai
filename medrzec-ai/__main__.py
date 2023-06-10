235bf3be21eb33644f8576ca615d0a0def7596a5
michalwa <michalwa2003@gmail.com>
2023-06-10 12:58:17 +0200

    Experiment with custom prompt template

--- medrzec-ai/__main__.py
+++ medrzec-ai/__main__.py
@@ -2,6 +2,22 @@ from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
+
+PROMPT_TEMPLATE = """
+The following is a friendly conversation between a human and an AI.
+The AI is talkative and provides lots of specific details from its context.
+If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Importantly, the AI wants to obtain answers to the following questions:
+- What is the human's name?
+- What is the weather like today where the human is?
+- What is he human's favourite type of soup?
+
+Current conversation:
+{history}
+Human: {input}
+AI:"""
 
 
 def main():
@@ -9,6 +25,9 @@ def main():
 
     llm = OpenAI()
     conversation = ConversationChain(
+        prompt=PromptTemplate(
+            input_variables=["history", "input"], template=PROMPT_TEMPLATE
+        ),
         llm=llm,
         memory=ConversationBufferMemory(),
     )
