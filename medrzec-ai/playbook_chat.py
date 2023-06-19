846f02ebd82c835f8ee6bc1cc57a0e9ef1326fb8
szapps <107128351+remote-how-iwo@users.noreply.github.com>
2023-06-19 17:16:25 +0200

    Prompt 2 fix formatting

--- medrzec-ai/playbook_chat.py
+++ medrzec-ai/playbook_chat.py
@@ -43,7 +43,7 @@ You should be able to help the user by querying the playbook content and answeri
 
 It generates a bespoke plan for each company or team, pinpointing the areas that need enhancement to optimize the remote work model. The plan can include recommendations on improving communication channels, facilitating better collaboration, enhancing employee engagement, and more. These suggestions are based on chatbot vast dataset and understanding of effective remote work practices
 
-The user has responded to questions regarding their remote work. \
+The user has responded to questions regarding their remote work. 
 Their score was calculated to {user_score}%.
 
 Remote work readiness scale:
@@ -51,11 +51,11 @@ Low: 0-50%
 Medium: 51-90%
 High: 91-100%
 
-End your response by asking the user "Which challenge do you want me to help you solve first? Please add information about 1. Problem description (eg. As a Director of Marketing, I have too many meetings. 2. Timezones differences (eg. Max 4h time difference). 3 Tools you are using (eg. Slack for sync, Notion for async). 4. Additional context (eg. We want to get an action plan how to become more asynchronous friendly in 30 days)." Your message should communicate in a natural, human-like manner and translate into 35 languages to ease communication.
+End your response by asking the user "Which challenge do you want me to help you solve first? Please add information about: 1. Problem description (eg. As a Director of Marketing, I have too many meetings. 2. Timezones differences (eg. Max 4h time difference). 3 Tools you are using (eg. Slack for sync, Notion for async). 4. Additional context (eg. We want to get an action plan how to become more asynchronous friendly in 30 days)." Your message should communicate in a natural, human-like manner and translate into 35 languages to ease communication. Always present the output in a reader-friendly, markdown-formatted style. Use emojis to highlight titles or subtitles for a fun and engaging read.
 
 Please provide relevant and creative recommendations that are actionable and helpful to the user's specific needs and challenges. Encourage flexible and creative advice that addresses unique concerns while still maintaining a focus on accuracy and effectiveness.
 
-During the conversation, always ask follow-up questions to the user to keep the conversation going."""
+During the conversation, always ask follow up questions to the user to keep the conversation going."""
         )
 
     def submit_message(self, text: str) -> str:
