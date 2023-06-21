3dea1896975ab99de42ca4f9c9b9bafb84e9b560
JelNiSław <jelnislaw@gmail.com>
2023-06-21 20:35:25 +0200

    Prompt improvements (#21)
    * Prompt v 1.1
    
    * Prompt engineering v 1.3
    
    * Prompt engineering v 1.3
    
    * Prompt engineering v 1.4
    
    * Prompt engineering v 1.6
    
    * Prompt engineering 1.6
    
    * Prompt engineering v 1.7
    
    * Prompt engineering v 1.8
    
    * Prompt engineering v 1.10
    
    * Prompt engineering v 1.11
    
    * Prompt engineering v 1.13
    
    * Prompt engineering 1.14
    
    * Prompt engineering
    
    * Prompt engineering 1.17
    
    * Prompt engineering 2
    
    * Prompt engineering fix
    
    * Prompt engineering fix
    
    * update prompt formatting
    
    * Prompt
    
    * Prompt simpler
    
    * Playbook new url added
    
    * Prompt 2 improvement
    
    * Prompt 2 big changes
    
    * prompt 1 fixes
    
    * prompt 2 fixing
    
    * Prompt 1 fixes
    
    * prompt 1 fixes
    
    * Prompt 1 fixing
    
    * prompt 2 fixing
    
    * Prompt 2 fixing
    
    * Prompt 2 fix formatting
    
    * Prompt #1 fixing
    
    * score text fixing
    
    * Prompt formatting
    
    * Move subprojects to other repositories (#20)
    
    * add a FastAPI API
    
    * separate the bot from the API
    
    * send API errors
    
    * add a Svelte web UI
    
    * move subprojects to other repositories
    
    ---------
    
    Co-authored-by: szapps <107128351+remote-how-iwo@users.noreply.github.com>

--- medrzec-ai/__main__.py
+++ medrzec-ai/__main__.py
@@ -1,84 +1,114 @@
 import asyncio
-import os
 import re
 from asyncio import Lock
+from uuid import uuid4
 
 import dotenv
-from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
-from slack_bolt.async_app import AsyncApp
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from .playbook_chat import PlaybookChat
 from .question_chat import QuestionChat
 
-PLAYBOOK_URL = "https://remotehow.notion.site/Remote-First-Work-Playbook-Template-b7a8b8437a3e4c22b68bc20e18bbd34d"
+PLAYBOOK_URL = "https://remotehow.notion.site/Remote-Work-Playbook-Template-b537fb9b503f4a0a9296774d464777d6"
 PLAYBOOK_UPSELL = f"<{PLAYBOOK_URL}|Get access to the world’s best playbook on #remotework, and improve your score.>\nLet’s dive in 🚀"
-
-dotenv.load_dotenv()
 
 active_conversations: dict[str, tuple[QuestionChat | PlaybookChat, Lock]] = {}
 
-app = AsyncApp(token=os.getenv("SLACK_BOT_TOKEN"))
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 
 
-@app.command("/start-chat")
-async def on_start(ack, body, respond):
-    await ack()
+class StartChatResponse(BaseModel):
+    chat_id: str
+    message: str
 
+
+class SendMessageRequest(BaseModel):
+    chat_id: str
+    user_message: str
+
+
+class SendMessageResponse(BaseModel):
+    messages: list[str]
+
+
+async def start_chat(conversation_id: str) -> str:
     lock = asyncio.Lock()
     async with lock:
         chat = QuestionChat()
-        active_conversations[body["user_id"]] = (chat, lock)
+        active_conversations[conversation_id] = (chat, lock)
         answer = await asyncio.to_thread(chat.start_conversation)
 
-    await respond(answer)
+    return answer
 
 
-@app.event("message")
-async def on_message(message, say):
-    user_id = message["user"]
-    if user_id not in active_conversations:
-        await say("Start a chat with the /start-chat command.")
-        return
+async def user_message(conversation_id: str, message: str) -> tuple[str, str | None]:
+    if conversation_id not in active_conversations:
+        raise HTTPException(404, "This conversation doesn't exist.")
 
-    chat, lock = active_conversations[user_id]
+    chat, lock = active_conversations[conversation_id]
 
     if lock.locked():
-        await say("Please wait for the previous answer.")
-        return
+        raise HTTPException(429, "Please wait for the previous answer.")
 
     async with lock:
-        answer = await asyncio.to_thread(chat.submit_message, message["text"])
+        answer = await asyncio.to_thread(chat.submit_message, message)
 
         if match := re.search(r"Score: (\d+)", answer, re.IGNORECASE):
             user_score = int(match[1])
-            message = f"*Your Remote Work Score is {user_score}%!* "
 
-            if user_score > 90:
-                message += """🧠
-You are a REMOTE PRO — super well done! ⭐ ⭐ ⭐ Keep rocking!"""
-
-            elif user_score > 50:
-                message += f"""👏 👏 👏
-You are familiar with remote work but need more guidance to feel 100% comfortable in it. Let us help you! 🏗️
-{PLAYBOOK_UPSELL}"""
-
-            else:
-                message += f"""😅
-You need more assistance with remote work to feel 100% comfortable in it. Let us help you! 🏗️
-{PLAYBOOK_UPSELL}"""
-
-            await say(message)
+            score_message = score_to_message(user_score)
 
             chat = PlaybookChat()
-            active_conversations[user_id] = (chat, lock)
+            active_conversations[conversation_id] = (chat, lock)
             answer = await asyncio.to_thread(chat.start_conversation, user_score)
 
-    await say(answer)
+        else:
+            score_message = None
+
+    return answer, score_message
 
 
-async def main():
-    await AsyncSocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start_async()
+@app.post("/start-chat", response_model=StartChatResponse)
+async def start_conversation():
+    chat_id = uuid4().hex
+    answer = await start_chat(chat_id)
+    return StartChatResponse(chat_id=chat_id, message=answer)
+
+
+@app.post("/send-message", response_model=SendMessageResponse)
+async def send_message(request: SendMessageRequest):
+    answer, score_message = await user_message(request.chat_id, request.user_message)
+    messages = [answer]
+
+    if score_message is not None:
+        messages.append(score_message)
+
+    return SendMessageResponse(messages=messages)
+
+
+def score_to_message(score: int) -> str:
+    message = f"*Your Remote Work Score is {score}%!* "
+
+    if score > 90:
+        message += """🧠
+You are a REMOTE PRO — super well done! ⭐⭐⭐ Keep rocking!"""
+
+    elif score > 50:
+        message += f"""👏👏👏
+You are familiar with remote work but need more guidance to feel fully comfortable in it. Let us help you! 🏗️
+{PLAYBOOK_UPSELL}"""
+
+    else:
+        message += f"""😅
+You need more assistance with remote work to feel fully comfortable in it. Let us help you! 🏗️
+{PLAYBOOK_UPSELL}"""
+    return message
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    dotenv.load_dotenv()
+    uvicorn.run(app)
--- medrzec-ai/playbook_chat.py
+++ medrzec-ai/playbook_chat.py
@@ -37,11 +37,13 @@ class PlaybookChat:
 
     def start_conversation(self, user_score: int) -> str:
         return self.agent.run(
-            input=f"""Help the user by querying the playbook and answering their questions. \
-The user is a People Manager at a company. \
-You are a very conversational AI. Keep the conversation going.
+            input=f"""As an AI-powered chatbot, your goal is to help people managers become better at leading distributed teams. Your task is to help the user generate a bespoke plan for each company or team, pinpointing the areas that need improvement to optimize the remote work model. The plan can include recommendations on communication channels, collaboration, employee engagement, and more, based on chatbot vast dataset and understanding of effective remote work practices.
 
-The user has responded to questions regarding their remote work. \
+You should be able to help the user by querying the playbook content and answering their questions or requests for help based on the playbook content or best practices from the world's top remote companies, such as GitLab, Doist, Buffer, or Automattic.
+
+It generates a bespoke plan for each company or team, pinpointing the areas that need enhancement to optimize the remote work model. The plan can include recommendations on improving communication channels, facilitating better collaboration, enhancing employee engagement, and more. These suggestions are based on chatbot vast dataset and understanding of effective remote work practices
+
+The user has responded to questions regarding their remote work. 
 Their score was calculated to {user_score}%.
 
 Remote work readiness scale:
@@ -49,7 +51,15 @@ Low: 0-50%
 Medium: 51-90%
 High: 91-100%
 
-End the response with a call to action encouraging the user to ask insightful questions."""
+End your response by asking the user:
+
+"Which challenge do you want me to help you solve first?/ 
+
+Please add information about: 1. Problem description (eg. As a Director of Marketing, I have too many meetings. 2. Timezones differences (eg. Max 4h time difference). 3 Tools you are using (eg. Slack for sync, Notion for async). 4. Additional context (eg. We want to get an action plan how to become more asynchronous friendly in 30 days)." Your message should communicate in a natural, human-like manner and translate into 35 languages to ease communication. Always present the output in a reader-friendly, markdown-formatted style. Use emojis to highlight titles or subtitles for a fun and engaging read.
+
+Please provide relevant and creative recommendations that are actionable and helpful to the user's specific needs and challenges. Encourage flexible and creative advice that addresses unique concerns while still maintaining a focus on accuracy and effectiveness.
+
+During the conversation, always ask follow-up questions to the user to keep the conversation going."""
         )
 
     def submit_message(self, text: str) -> str:
